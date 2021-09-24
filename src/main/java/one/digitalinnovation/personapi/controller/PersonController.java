80a4eb1c6d38e448953166d905e053987d83e649
B.Z <brunozarth@gmail.com>
2021-09-24 13:53:15 -0300

    add files MessageResponseDTO and PersonRepository with PersonController interactions

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,15 +1,36 @@
 package one.digitalinnovation.personapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import one.digitalinnovation.personapi.dto.MessageResponseDTO;
+import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.repository.PersonRepository;
 
 @RestController
 @RequestMapping(path = "/api/v1/people")
 public class PersonController {
 	
+	private PersonRepository personRepository;
+	
+	@Autowired
+	public PersonController(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
+	
+	public MessageResponseDTO createPerson(@RequestBody Person person) {
+		Person savedPerson = personRepository.save(person);
+		return MessageResponseDTO
+				.builder()
+				.message("Created person with id " + savedPerson.getId())
+				.build();
+	}
+	
 	@GetMapping
 	public String getBook() {
-		return "<h1>ô cara de cavalô-ou!";
+		return "<h1>it's working!";
 	}
 }
